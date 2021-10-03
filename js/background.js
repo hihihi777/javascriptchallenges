a59d542d78386f4a6c3b671add087507ea6830e0
hihihi777 <qnalswkd@gmail.com>
2021-10-03 15:42:10 +0900

    hahaha

--- js/background.js
+++ js/background.js
@@ -2,18 +2,8 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const body = document.querySelector(".body");
-
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
-
-function background() {
-    document.body.style.background_img = chosenImage;
-}
-
-body.addEventListener("submit", background);
-
-bgImage.className = bgImage;
