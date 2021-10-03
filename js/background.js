66ba35a6d7cc809307ed8774b484a1c7a3d65d6f
hihihi777 <qnalswkd@gmail.com>
2021-10-03 14:32:54 +0900

    background-idonknow

--- js/background.js
+++ js/background.js
@@ -2,8 +2,16 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
+const body = document.querySelector(".body");
+
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+
+function background() {
+    document.body.style.background = chosenImage;
+}
+
+body.addEventListener("submit", background);
